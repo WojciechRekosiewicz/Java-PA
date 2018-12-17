3e1b361c2d364b0405b6e9563422410765c97deb
WojciechRekosiewicz <wojtek24a@gmail.com>
2018-12-17 12:27:38 +0100

    view added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import view.View;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        View view = new View();
+        view.menu();
     }
 }
